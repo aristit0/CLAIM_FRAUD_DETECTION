a50002e8f06ff6cf0e61f2f1995429376513ad09
aristit0sugihharto <aris.tito04@mastersystem.co.id>
2025-11-28 17:55:48 +0700

    add etl script

--- ETL/etl_claim_feature_set.py
+++ ETL/etl_claim_feature_set.py
@@ -15,9 +15,6 @@ spark = (
     .appName("claim_feature_set_etl")
     .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
     .config("spark.sql.catalog.spark_catalog.type", "hive")
-    .config("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog")
-    .config("spark.sql.catalog.local.type", "hadoop")
-    .config("spark.sql.catalog.local.warehouse", "hdfs:///warehouse/tablespace/external/hive")
     .getOrCreate()
 )
 
