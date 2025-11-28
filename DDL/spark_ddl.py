2997878a2e2dd7bb875addc9771da8f881d34613
aristit0sugihharto <aris.tito04@mastersystem.co.id>
2025-11-28 17:17:03 +0700

    add etl script

--- DDL/spark_ddl.py
+++ DDL/spark_ddl.py
@@ -1,45 +1,75 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.getOrCreate()
+spark = (
+    SparkSession.builder
+    .appName("CreateClaimFeatureSetTable")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
+    .config("spark.sql.catalog.spark_catalog.type", "hive")
+    .config("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog")
+    .config("spark.sql.catalog.local.type", "hadoop")
+    .config("spark.sql.catalog.local.warehouse", "hdfs:///warehouse/tablespace/external/hive")
+    .getOrCreate()
+)
+
+print("Spark started")
+
+
+# ====================================================================
+#  CREATE ICEBERG TABLE (WITH LOCATION + FORMAT-VERSION = 2)
+# ====================================================================
 
 spark.sql("""
 DROP TABLE IF EXISTS iceberg_curated.claim_feature_set
 """)
 
 spark.sql("""
-CREATE EXTERNAL TABLE iceberg_curated.claim_feature_set (
+CREATE TABLE iceberg_curated.claim_feature_set (
     claim_id BIGINT,
+
     patient_nik STRING,
     patient_name STRING,
     patient_gender STRING,
     patient_dob DATE,
     patient_age INT,
+
     visit_date DATE,
     visit_day INT,
     visit_type STRING,
     doctor_name STRING,
     department STRING,
+
     icd10_primary_code STRING,
     icd10_primary_desc STRING,
+
     procedures_icd9_codes ARRAY<STRING>,
     procedures_icd9_descs ARRAY<STRING>,
+
     drug_codes ARRAY<STRING>,
     drug_names ARRAY<STRING>,
+
     vitamin_names ARRAY<STRING>,
+
     total_procedure_cost DOUBLE,
     total_drug_cost DOUBLE,
     total_vitamin_cost DOUBLE,
     total_claim_amount DOUBLE,
+
     tindakan_validity_score DOUBLE,
     obat_validity_score DOUBLE,
     vitamin_relevance_score DOUBLE,
     biaya_anomaly_score DOUBLE,
+
     rule_violation_flag INT,
     rule_violation_reason STRING,
+
     created_at TIMESTAMP
 )
 USING iceberg
-PARTITIONED BY (visit_year INT, visit_month INT)
-TBLPROPERTIES ('format-version'='2')
-"""
+PARTITIONED BY (visit_year, visit_month)
+TBLPROPERTIES (
+    'format-version' = '2',
+    'location' = 'hdfs:///warehouse/tablespace/external/hive/iceberg_curated.db/claim_feature_set'
 )
+""")
+
+print("Iceberg table 'claim_feature_set' created successfully.")
